5698c3c2833dbb04bb19ba1a2f52a10b48f501a4
andreirubaniak <andreirubaniak@gmail.com>
2020-09-13 16:04:10 +0300

    Модуль 1

---
+++ task1_4.js
@@ -0,0 +1,35 @@
+const orderPieces = 4;
+
+const credits = 23580;
+const pricePerDroid = 3000;
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_DENIED = 'Недостаточно средств на счету!';
+
+let totalPrice = pricePerDroid * orderPieces; // Write code on this line
+let balanceCredit = Number(credits - totalPrice); // Write code on this line
+let message;
+
+// Write code under this line
+if (balanceCredit > 0) {
+  message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
+} else if (balanceCredit < 0) {
+  message = ACCESS_DENIED;
+}
+
+if (orderPieces === null) {
+  message = CANCELED_BY_USER;
+}
+
+console.log(message);
+
+//если orderPieces равно 4
+// то значение message будет равно
+// 'Вы купили 4 дроидов, на счету осталось 11580 кредитов'
+
+//если orderPieces равно null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если orderPieces равно 10
+// то значение message будет равно
+// 'Недостаточно средств на счету!'
